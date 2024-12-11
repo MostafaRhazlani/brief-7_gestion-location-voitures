83368bf256cef6252e617cc34f1c3f154b0274d9
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-11 16:50:48 +0100

    add ui page voitures

--- views/layout/_SIDEBAR.php
+++ views/layout/_SIDEBAR.php
@@ -30,7 +30,7 @@
         <?php echo "<img class='p-1 bg-emerald-600 rounded-md mr-4' width='26' src='../../src/img/images_sidebar/users.svg' alt=''>" ?>
             Clients
         </a>
-        <a class="p-2 mb-1 hover:bg-[#595480] rounded-sm flex items-center" href="#">
+        <a class="p-2 mb-1 hover:bg-[#595480] rounded-sm flex items-center <?php if($page == '/views/voitures/voitures.php') echo 'bg-[#595480]' ?>" href="<?php $URL ?>/views/voitures/voitures.php">
             <?php echo "<img class='p-1 bg-cyan-600 rounded-md mr-4' width='27' src='../../src/img/images_sidebar/car.svg' alt=''>" ?>
             Cars
         </a>
---
+++ views/voitures/voitures.php
@@ -0,0 +1,83 @@
+<?php 
+    require_once('../../connectdb/connectiondb.php');
+    
+    // select table users
+    $voitures = "SELECT * FROM voitures";
+    $resultVoitures = $conn->query($voitures);
+?>
+
+<?php include('../layout/_HEAD.php'); ?>
+
+<div class="w-full h-screen grid grid-cols-5 grid-rows-10 p-2 gap-2">
+    <!-- include sidebar -->
+    <?php include('../layout/_SIDEBAR.php') ?>
+
+    <header class="col-span-4 row-span-1 py-4 px-6 bg-[#2a2455] rounded-md flex justify-end">
+        <input class="p-2 w-2/5 rounded-md" type="search" placeholder="Search...">
+    </header>
+
+    <div class="col-span-4 row-span-1 py-4 px-6 bg-[#2a2455] rounded-md text-white flex justify-between">
+        <button class="showFormAdd py-2 px-4 bg-[#423c6b] rounded-md hover:bg-[#5b5680]"><i class="fa-solid fa-user-plus"></i> Add Client</button>
+        <button class="py-2 px-4 bg-[#423c6b] rounded-md hover:bg-[#5b5680]"><i class="fa-solid fa-arrow-down-a-z"></i> Sort Clients</button>
+    </div>
+    
+    <div class="col-span-4 row-span-8 flex justify-center">
+        <div class="w-full bg-[#2a2455] p-10 rounded-md">
+            <table class="w-full mx-auto table-auto text-center text-gray-300">
+                <thead>
+                    <tr class="bg-[#191444]">
+                        <th class="p-4">ID</th>
+                        <th class="p-4">N° Immatriculation</th>
+                        <th class="p-4">Marque</th>
+                        <th class="p-4">Modele</th>
+                        <th class="p-4">Anne</th>
+                        <th class="p-4">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- display all users -->
+                    <?php if($resultVoitures) { ?>
+                        <?php while($voiture = $resultVoitures->fetch()) { ?>
+                            <tr class="border-t-[0.2px] border-gray-500 hover:bg-[#585286]">
+                                <td class="px-2 py-4"><?php echo $voiture['id'] ?></td>
+                                <td class="px-2 py-4"><?php echo $voiture['numImmatriculation'] ?></td>
+                                <td class="px-2 py-4"><?php echo $voiture['marque'] ?></td>
+                                <td class="px-2 py-4"><?php echo $voiture['modele'] ?></td>
+                                <td class="px-2 py-4"><?php echo $voiture['annee'] ?></td>
+                                <td class="px-2 py-4">
+                                    <a href="./users.php?idEditUser=<?php echo $client['id'] ?>" class="showFormEdit bg-blue-700 rounded-full px-2 py-1 text-white text-[13px] hover:bg-blue-500 mr-2">
+                                        <i class="fa-solid fa-user-pen"></i>&nbsp;Edit
+                                    </a>
+                                    <a href="./users.php?idDeleteUser=<?php echo $client['id'] ?>" class="showFormDelete bg-red-700 rounded-full px-2 py-1 text-white text-[13px] hover:bg-red-500 cursor-pointer">
+                                        <i class="fa-solid fa-user-minus"></i>&nbsp;Delete
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+        <?php include('./addVoiture.php') ?>
+        <?php include('../../alertAdd.php') ?>
+    </div>
+</div>
+
+<?php 
+    
+    include('../layout/_FOOTER.php');
+    if(isset($_GET['alert'])) {
+        $alert = $_GET['alert'];
+
+        if($alert == 'success_add') {
+            echo "<script>
+                const showAlertAdd = document.querySelector('.showAlertAdd');
+                showAlertAdd.classList.remove('hidden')
+
+                setTimeout(() => {
+                    window.location.href = 'voitures.php'
+                }, 3000)
+            </script>";
+        }
+    }
+?>
