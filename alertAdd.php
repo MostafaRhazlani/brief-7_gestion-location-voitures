5c8e9471eb05cb12a6768fca7201b8400be02e16
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-11 16:51:31 +0100

    add part insert to insert new voiture

--- alertAdd.php
+++ alertAdd.php
@@ -1,3 +1,5 @@
+<?php $page = $_SERVER['PHP_SELF'] ?>
+
 <div class="showAlertAdd absolute z-10 w-1/4 bg-white p-5 top-20 hidden rounded-md text-center">
     
     <div class="w-full flex justify-center mb-10 mt-5">
@@ -5,6 +7,11 @@
             <span class="text-6xl text-green-500"><i class="fa-solid fa-check"></i></span>
         </div>
     </div>
-
-    <h1 class="text-2xl font-semibold text-center mb-10">Client added succesfuly</h1>
+    <?php 
+        if($page == '/views/users/users.php') {
+            echo "<h1 class='text-2xl font-semibold text-center mb-10'>Client added succesfuly</h1>";
+        } else if($page == '/views/voitures/voitures.php') {
+            echo "<h1 class='text-2xl font-semibold text-center mb-10'>Voiture added succesfuly</h1>";
+        }
+    ?>
 </div>
---
+++ views/voitures/insertVoiture.php
@@ -0,0 +1,16 @@
+<?php
+    require_once('../../connectdb/connectiondb.php');
+    // insert new client
+    $immatriculation = isset($_POST['immatriculation']) ? $_POST['immatriculation'] : "";
+    $marque = isset($_POST['marque']) ? $_POST['marque'] : "";
+    $modele = isset($_POST['modele']) ? $_POST['modele'] : "";
+    $annee = isset($_POST['annee']) ? $_POST['annee'] : "";
+
+    $voitureQuery = "INSERT INTO voitures(numImmatriculation, marque, modele, annee) VALUES(?,?,?,?)";
+    $params = array($immatriculation, $marque, $modele, $annee);
+    
+    $resultVoitureQuery = $conn->prepare($voitureQuery);
+    if($resultVoitureQuery->execute($params)) {
+        header('location:voitures.php?alert=success_add');
+    }
+?>
