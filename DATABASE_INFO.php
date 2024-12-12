8b6c6b309c83ee8f409493bb38d700781ca90e91
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-12 16:38:55 +0100

    put variables in file connectiodb and delete file database info

--- DATABASE_INFO.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $servername = 'localhost';
-    $username = 'root';
-    $password = 'password';
-    $dbName = 'societe_location_voitures';
-
-?>
--- connectdb/connectiondb.php
+++ connectdb/connectiondb.php
@@ -1,5 +1,8 @@
 <?php
-    require_once('../../DATABASE_INFO.php');
+    $servername = 'localhost';
+    $username = 'root';
+    $password = 'password';
+    $dbName = 'societe_location_voitures';
 
     $conn = mysqli_connect($servername, $username, $password, $dbName);
 
